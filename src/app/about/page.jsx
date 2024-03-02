6ba02ca6bff637cf5f59438e0eb666ed69e2ae0f
mahesh <jadhao.mahesh1009@gmail.com>
2024-03-02 16:01:50 +0530

    create a about page

---
+++ src/app/about/page.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function About() {
+  return (
+    <div className='max-w-6xl mx-auto p-3 flex space-y-4 flex-col justify-start'>
+      <h1 className="text-2xl font-medium text-amber-600">About</h1>
+      <p>The moving images of a film are created by photographing actual scenes with a motion-picture camera, by photographing drawings or miniature models using traditional animation techniques, by means of CGI and computer animation, or by a combination of some or all of these techniques, and other visual effects.</p>
+      <p>Before the introduction of digital production, a series of still images were recorded on a strip of chemically sensitized celluloid (photographic film stock), usually at a rate of 24 frames per second. The images are transmitted through a movie projector at the same rate as they were recorded, with a Geneva drive ensuring that each frame remains still during its short projection time. A rotating shutter causes stroboscopic intervals of darkness, but the viewer does not notice the interruptions due to flicker fusion. The apparent motion on the screen is the result of the fact that the visual sense cannot discern the individual images at high speeds, so the impressions of the images blend with the dark intervals and are thus linked together to produce the illusion of one moving image. An analogous optical soundtrack (a graphic recording of the spoken words, music and other sounds) runs along a portion of the film exclusively reserved for it, and was not projected.</p>
+      <p>The art of film has drawn on several earlier traditions in fields such as oral storytelling, literature, theatre and visual arts. Forms of art and entertainment that had already featured moving or projected images include:
+
+shadowgraphy, probably used since prehistoric times
+camera obscura, a natural phenomenon that has possibly been used as an artistic aid since prehistoric times
+shadow puppetry, possibly originated around 200 BCE in Central Asia, India, Indonesia or China
+The magic lantern, developed in the 1650s. The multi-media phantasmagoria shows that magic lanterns were popular from 1790 throughout the first half of the 19th century and could feature mechanical slides, rear projection, mobile projectors, superimposition, dissolving views, live actors, smoke (sometimes to project images upon), odors, sounds and even electric shocks.
+</p>
+    </div>
+  )
+}
+
+export default About
